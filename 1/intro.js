095cadbe708b387833b924d149e1e16b355302f1
joseR946 <jose791946@gmail.com>
2019-12-27 13:52:16 -0300

    Nuevas funciones 27DIC19

--- 1/intro.js
+++ 1/intro.js
@@ -1,1 +1,24 @@
-alert("alerta");
+function abrir(){
+    var ventana=open();
+    ventana.document.write("estoy escribiendo en la nueva ventana <br> ");
+    ventana.document.write("Segunda Linea");
+}
+function abrirParametros(){
+    var ventana=open('','','status=yes, width=400, height=250, menubar=yes');
+    ventana.document.write("Esto es lo primero que aparece");
+}
+function mostrarAlerta(){
+    alert("esta ventana ya la utilizamos");
+}
+function confirmar(){
+    var respuesta=confirm("Presione alguno de los dos botones");
+    if(respuesta==true){
+        alert("PRECIONO ACEPTAR");
+    }else{
+        alert("PRECIONO CANCELAR");
+    }
+}
+function cargarCadena(){
+    var cad=prompt("Cargue una cadena", "");
+    alert("Usted ingreso: "+cad);
+}
